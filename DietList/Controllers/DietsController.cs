970a92bba8e33b3af30de7e498157b9b84477474
Min Chang <minchangmhc@gmail.com>
2021-06-08 10:38:39 -0700

    🚀 finish the controller. Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- DietList/Controllers/DietsController.cs
+++ DietList/Controllers/DietsController.cs
@@ -28,5 +28,76 @@ namespace Diet.Controllers
       }
       return await query.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Diet>> GetDiet(int id)
+    {
+      var diet = await _db.Diets.FindAsync(id);
+
+      if (diet == null)
+      {
+        return NotFound();
+      }
+
+      return diet;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Diet diet)
+    {
+      if (id != diet.DietId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(diet).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!DietExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Diet>> Post(Diet diet)
+    {
+      _db.Diets.Add(diet);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetDiet), new { id = diet.DietId }, diet);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDiet(int id)
+    {
+      var diet = await _db.Diets.FindAsync(id);
+      if (diet == null)
+      {
+        return NotFound();
+      }
+
+      _db.Diets.Remove(diet);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool DietExists(int id)
+    {
+      return _db.Diets.Any(e => eas.DietId == id);
+    }
   }
 }
--- DietList/Models/Diet.cs
+++ DietList/Models/Diet.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 namespace Diet.Models
 {
   public class Diet
   {
     public int DietId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
     public string Description { get; set; }
     public string Restriction { get; set; }
   }
